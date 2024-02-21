dc872b2f4e39bb6cde8b6bfb76ea8aff3bb6d40e
Vitaly <vtvz.ru@gmail.com>
2024-02-21 11:40:13 +0300

    Remove langmap as colemak-dh and qwerty are not friends

--- user/polish.lua
+++ user/polish.lua
@@ -1,31 +1,4 @@
-local function set_langmap()
-  local function escape(str)
-    -- Эти символы должны быть экранированы, если встречаются в langmap
-    local escape_chars = [[;,."|\]]
-    return vim.fn.escape(str, escape_chars)
-  end
-
-  -- Наборы символов, введенных с зажатым шифтом
-  local en_shift = [[~QWFPBJLUY:{}ARSTGMNEIO"ZXCDVKH<>]]
-  local ru_shift = [[ËЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ]]
-  -- Наборы символов, введенных как есть
-  -- Здесь я не добавляю ',.' и 'бю', чтобы впоследствии не было рекурсивного вызова комманды
-  local en = [[`qwfpbjluy;[]arstgmneio'zxcdvkh]]
-  local ru = [[ёйцукенгшщзхъфывапролджэячсмить]]
-
-  vim.opt.langmap = vim.fn.join({
-    --  ; - разделитель, который не нужно экранировать
-    --  |
-    escape(ru_shift)
-      .. ";"
-      .. escape(en_shift),
-    escape(ru) .. ";" .. escape(en),
-  }, ",")
-end
-
 return function()
-  set_langmap()
-
   vim.filetype.add({
     extension = {
       tf = "terraform",
